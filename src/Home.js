663e6c8d3dd27e378dc0e58266735dfb1de7c9e4
Sze Tyng <lee.szetyng@gmail.com>
2023-05-27 16:05:36 +0800

    Fix changes with env variables

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,11 @@
 import React from "react";
 
+<<<<<<< HEAD
 export default function Home(){
     return <div className="tw-font-black tw-p-2">something</div>
 }
+=======
+export default function Home() {
+  return <div>something</div>;
+}
+>>>>>>> 5d37e50 (Fix changes with env variables)
