b2f0b87743d79f0997690ec1a2fb1aa46373d8bb
Sze Tyng <lee.szetyng@gmail.com>
2023-05-27 23:30:59 +0800

    Add All Transactions table

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,15 @@
 import React, { useState, useEffect } from "react";
 import AppBar from "./components/AppBar";
 import CardScore from "./components/CardScore";
-import { Col, Row } from "antd";
+
+import { Col, Row, Card, Table, Tag } from "antd";
 
 import { fetchUserTransactions } from "./utils/api";
 // fetchUserTransactions("970417-07-3958");
 
 export default function Home() {
   const [searchValue, setSearchValue] = useState("");
-  const [userTxns, setUserTxns] = useState(null);
+  const [userTxns, setUserTxns] = useState([]);
 
   const handleSearch = (searchValue) => {
     setSearchValue(searchValue);
@@ -28,6 +29,52 @@ export default function Home() {
     fetchData();
   }, [searchValue]);
 
+  const allTxnsColumns = [
+    {
+      title: "Transaction ID",
+      dataIndex: "transaction_id",
+      key: "transaction_id",
+    },
+    {
+      title: "Transaction Details",
+      dataIndex: "transaction_details",
+      key: "transaction_details",
+    },
+    {
+      title: "Date",
+      dataIndex: "transaction_date",
+      key: "transaction_date",
+    },
+    {
+      title: "Transacted With",
+      dataIndex: "transacted_to",
+      key: "transacted_to",
+    },
+    {
+      title: "Amount",
+      dataIndex: "transaction_amount",
+      key: "transaction_amount",
+    },
+    {
+      title: "Platform",
+      dataIndex: "client_id",
+      key: "client_id",
+    },
+    // TODO: fix this
+    // {
+    //   title: "Tags",
+    //   dataIndex: "anomaly_type",
+    //   key: "anomaly_type",
+    //   render: (tags) => (
+    //     <>
+    //       {tags.map((tag) => (
+    //         <Tag key={tag}>{tag}</Tag>
+    //       ))}
+    //     </>
+    //   ),
+    // },
+  ];
+
   return (
     <div>
       <AppBar onSearch={handleSearch} />
@@ -45,6 +92,29 @@ export default function Home() {
           <CardScore metric="Total Outflow" value="100" />
         </Col>
       </Row>
+
+      <Card>
+        <p className="tw-text-lg">All Transactions</p>
+        <Table
+          columns={allTxnsColumns}
+          dataSource={userTxns.map((obj, index) => {
+            return { ...obj, key: index + 1 };
+          })}
+          expandable={{
+            expandedRowRender: (record) => (
+              <p
+                style={{
+                  margin: 0,
+                }}
+              >
+                {record.description}
+              </p>
+            ),
+            rowExpandable: (record) =>
+              record.description !== "No transaction flow found.",
+          }}
+        />
+      </Card>
     </div>
   );
 }
