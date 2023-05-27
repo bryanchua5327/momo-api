ec76c01ea3518d59c96178f8d8164320dac59d79
KwanYee Tan <tkyee1998@gmail.com>
2023-05-27 21:42:39 +0800

    Added graph

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,11 @@
 import React from "react";
+import NodeGraph from "./components/nodeGraph";
 
+<<<<<<< HEAD
 export default function Home() {
   return <div className="tw-font-black tw-p-2">something</div>;
+=======
+export default function Home(){
+    return <NodeGraph></NodeGraph>
+>>>>>>> 42f7e16 (Added graph)
 }
