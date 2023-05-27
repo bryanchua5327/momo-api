112833777ee20998b7836a29856c04b85227002f
KwanYee Tan <tkyee1998@gmail.com>
2023-05-27 16:09:27 +0800

    adding tailwind colors

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Home from "./Home"
+import Splitter from "./pages/billSplitter"
 import { Routes, Route } from "react-router-dom"
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={ <Home/> } />
+        <Route path="/splitter" element={ <Splitter/> } />
       </Routes>
     </div>
   )
